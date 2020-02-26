fdef159439684356aa5d922cb8e7a76a5404d96e
emrum01 <emrum01@gmail.com>
2020-02-27 00:38:49 +0900

    tried to remove global variables but I gave up.

--- roulette.js
+++ roulette.js
@@ -1,5 +1,5 @@
-const themeNum = 24;
-const adjustNum = 1;
+const THEMENUM = 24;
+const ADJUSTNUM= 1;
 var flag = 0;
 var paraflag = 0;
 var allTalkList = new Array();
@@ -31,19 +31,19 @@ var talkList = ["死ぬまでに絶対行ってみたい場所",
     "16200円あったら何が出来るか"];
 
     var para = ()=>{
-        var paraValue= talkList[getRandom(adjustNum,themeNum)];
+        var paraValue= talkList[getRandom(ADJUSTNUM,THEMENUM)];
         document.form1.text1.value=paraValue;
         paraflag = 1;
     }
     var paraslow = ()=>{
         clearTimeout(st);
-        var paraValue= talkList[getRandom(adjustNum,themeNum)];
+        var paraValue= talkList[getRandom(ADJUSTNUM,THEMENUM)];
         document.form1.text1.value=paraValue;
         console.log("paraslow");
     }
     var parastop = ()=>{
         clearTimeout(ss);
-        var paraValue= talkList[getRandom(adjustNum,themeNum)];
+        var paraValue= talkList[getRandom(ADJUSTNUM,THEMENUM)];
         document.form1.text1.value=paraValue;
         console.log("parastop");
     }
@@ -53,13 +53,23 @@ function getRandom( min, max ) {
         var random = Math.floor( Math.random() * (max + 1 - min) ) + min;
         return random;
 }
+/*
+function closure(s){
+    var start = s
+    return start;
+}
+*/
+//スタートを押すとルーレットが回り、再度押すと徐々に止まる
+//function startRoulette1(){
 function startRoulette(){
     if(flag==0){
         flag=1;
         document.form1.btn.value="ストップ";
         //allTalkList.push(start=setInterval('para()',50));
-        var start=setInterval('para()',50);
+        start = setInterval('para()',50);
+        //start = closure(setInterval('para()',50));
         console.log("paraflag = "+ paraflag);
+        //return start;
     } else{
         flag=0;
         console.log(allTalkList.length);
@@ -69,14 +79,15 @@ function startRoulette(){
         st=setTimeout('paraslow()',500);
         st=setTimeout('paraslow()',900);
         ss=setTimeout('parastop()',999);
-        var paraValue = talkList[getRandom(adjustNum,themeNum)];
+        var paraValue = talkList[getRandom(ADJUSTNUM,THEMENUM)];
         document.form1.text1.value=paraValue;
         document.form1.btn.value="スタート";
     }
 }
+//var start = closure(startRoulette2());
+//startRoulette2(start());
 
-//本当に望んでいるものは表に出さない
-
+//}
 //改善:アロー関数は処理が動的なのでslotStart関数の上に配置
 //問題:ルーレットストップせず
 //対策: getRandom関数をコールバックにしてみる
@@ -88,5 +99,16 @@ function startRoulette(){
     //これから:setIntervalは複数存在する(実行される度に増える)
             //ので、全てをclearしてやればいい
 //できた！！
+//なんで？：arrayがグローバルスコープだったから。
+//結局スコープの問題だったぽい。なんだよチキショー。
 
-//
+//グローバルスコープを使わないでやってみたい。
+//問題: if分岐は処理完了時に変数が消えてしまう
+//対策:ネストして外側の関数に変数をおいて実行したら？
+//結果:結局ネストしたものも消える
+//対策:どうにかクロージャにぶち込む
+//結果:無理っぽい諦めた。。
+//対策:ネストしてクロージャ
+//結果:外側での処理が猥雑になり、結局ネストしている関数でif分岐を使うしかなかった。
+//ひとまず、諦めた。
+
